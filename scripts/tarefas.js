9282335e73de81ffc19bb20cb9cc433dc2438596
unknown <joaopaulo.a.barreira@gmail.com>
2022-07-06 22:38:28 -0300

    primeira parte tarefas.js -getMe

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -1,3 +1,32 @@
 let token = localStorage.getItem('token')
+let userInfo = document.querySelector('.user-info')
+var requestHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': token
+}
 
-console.log(token)
+var requestPostConfiguration = {
+    headers: requestHeaders,
+}
+
+
+
+function getUser(){
+    fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe',requestPostConfiguration).then(
+        response =>{
+            response.json().then(
+                sucess =>{
+                   
+                    userInfo.children[0].innerHTML = `<p>${sucess.firstName}</p>`
+                
+                })
+        
+                })
+        }
+    
+
+getUser()
+
+
+
